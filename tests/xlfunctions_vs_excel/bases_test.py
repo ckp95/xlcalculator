59f215237ed4961f8c58ceccc20008e5c4b1784e
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2020-12-26 17:00:41 +0000

    remove unneeded comments

--- tests/xlfunctions_vs_excel/bases_test.py
+++ tests/xlfunctions_vs_excel/bases_test.py
@@ -1,17 +1,4 @@
-# import pytest
-# from _pytest.mark.structures import MarkDecorator
-# from typing import Optional, Callable
-
-# from xlcalculator.model import ModelCompiler
-# from xlcalculator.evaluator import Evaluator
-# from .. import testing
-# from ..case import Case, parametrize_cases
-
-
 from ..testing import workbook_test_cases, assert_equivalent
-
-
-
 
 
 @workbook_test_cases("BASES.xlsx")
