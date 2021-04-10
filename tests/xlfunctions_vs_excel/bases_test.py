0fe8f10dc9ca92d9039d82bc248aaeeb10a6dbd5
Cameron Pinnegar <cameron.pinnegar@education.gov.uk>
2021-04-10 20:06:58 +0100

    add property tests that base functions are inverses of each other

--- tests/xlfunctions_vs_excel/bases_test.py
+++ tests/xlfunctions_vs_excel/bases_test.py
@@ -6,7 +6,18 @@ from hypothesis.strategies import integers, floats, one_of, none, text
 
 from tests.testing import assert_equivalent, Case, parametrize_cases
 from tests.conftest import formula_env
-from xlcalculator.xlfunctions.math import DEC2BIN, DEC2OCT, DEC2HEX, BIN2OCT, BIN2DEC, BIN2HEX, OCT2BIN, OCT2DEC, OCT2HEX, HEX2BIN
+from xlcalculator.xlfunctions.math import (
+    DEC2BIN,
+    DEC2OCT,
+    DEC2HEX,
+    BIN2OCT,
+    BIN2DEC,
+    BIN2HEX,
+    OCT2BIN,
+    OCT2DEC,
+    OCT2HEX,
+    HEX2BIN
+)
 
 
 MAX_EXAMPLES = 1000
@@ -96,11 +107,11 @@ def test_dec2hex_with_places(env_dec2hex_places):
     fuzz_scalars(env=env_dec2hex_places, variables=variables)
     
 
-def binary_numbers_as_strings():
+def binary_numbers_as_strings(max_size):
     return text(
         alphabet=set("01"),
         min_size=1,
-        max_size=11
+        max_size=max_size
     ).filter(lambda x: x == "0" or x[0] != "0")
 
 
@@ -108,7 +119,7 @@ env_bin2oct = formula_env(BIN2OCT, "number")
 
 def test_bin2oct(env_bin2oct):
     variables = given(
-        number=one_of(xl_numbers(), binary_numbers_as_strings()) 
+        number=one_of(xl_numbers(), binary_numbers_as_strings(11)) 
     )
     fuzz_scalars(env=env_bin2oct, variables=variables)
 
@@ -118,7 +129,7 @@ env_bin2oct_places = formula_env(BIN2OCT, ["number", "places"])
 
 def test_bin2oct_with_places(env_bin2oct_places):
     variables = given(
-        number=one_of(xl_numbers(), binary_numbers_as_strings()),
+        number=one_of(xl_numbers(), binary_numbers_as_strings(11)),
         places=xl_numbers(-5, 15)
     )
     fuzz_scalars(env=env_bin2oct_places, variables=variables)
@@ -129,7 +140,7 @@ env_bin2dec = formula_env(BIN2DEC, "number")
 
 def test_bin2dec(env_bin2dec):
     variables = given(
-        number=one_of(xl_numbers(), binary_numbers_as_strings()) 
+        number=one_of(xl_numbers(), binary_numbers_as_strings(11)) 
     )
     fuzz_scalars(env=env_bin2dec, variables=variables)
     
@@ -142,7 +153,7 @@ env_bin2hex = formula_env(BIN2HEX, "number")
 
 def test_bin2hex(env_bin2hex):
     variables = given(
-        number=one_of(xl_numbers(), binary_numbers_as_strings()) 
+        number=one_of(xl_numbers(), binary_numbers_as_strings(11)) 
     )
     fuzz_scalars(env=env_bin2hex, variables=variables)
     
@@ -152,7 +163,7 @@ env_bin2hex_places = formula_env(BIN2HEX, ["number", "places"])
 
 def test_bin2hex_with_places(env_bin2hex_places):
     variables = given(
-        number=one_of(xl_numbers(), binary_numbers_as_strings()),
+        number=one_of(xl_numbers(), binary_numbers_as_strings(11)),
         places=xl_numbers(-5, 15)
     )
     fuzz_scalars(env=env_bin2hex_places, variables=variables)
@@ -248,6 +259,69 @@ def test_hex2bin_with_places(env_hex2bin_places):
         places=xl_numbers(-5, 15)
     )
     fuzz_scalars(env=env_hex2bin_places, variables=variables)
+    
+
+def to_bin(x):
+    return bin(x)[2:]
+
+def to_oct(x):
+    return oct(x)[2:]
+
+def to_hex(x):
+    return hex(x)[2:]
+
+def ten_chars_or_fewer(x):
+    return len(x) <= 10
 
 
-# todo: put max parameter on the binary strings too
+@given(binary_string=integers(min_value=0).map(to_bin).filter(ten_chars_or_fewer))
+@settings(max_examples=MAX_EXAMPLES)
+def test_bin2oct_and_oct2bin_are_inverses(binary_string):
+    # Note that in Python, bin(1023) == "0b1111111111", but in Excel, DEC2BIN(1023) gives
+    # a #NUM! error while BIN2DEC(1111111111) == -1. So the numbers in the @given
+    # decorator are not the same as how Excel interprets the input. This test just says
+    # that OCT2BIN and BIN2OCT are inverses of each other, it says nothing about what
+    # actual values they produce or what they mean. The same is true of the other inverse
+    # property tests later on in this file. 
+    assert binary_string == OCT2BIN(BIN2OCT(binary_string))
+
+
+@given(binary_string=integers(min_value=0).map(to_bin).filter(ten_chars_or_fewer))
+@settings(max_examples=MAX_EXAMPLES)
+def test_bin2hex_and_hex2bin_are_inverses(binary_string):   
+    assert binary_string == HEX2BIN(BIN2HEX(binary_string))
+        
+
+@given(binary_string=integers(min_value=0).map(to_bin).filter(ten_chars_or_fewer))
+@settings(max_examples=MAX_EXAMPLES)
+def test_bin2dec_and_dec2bin_are_inverses(binary_string):
+    assert binary_string == DEC2BIN(BIN2DEC(binary_string))
+    
+
+@given(octal_string=integers(min_value=0).map(to_oct).filter(ten_chars_or_fewer))
+@settings(max_examples=MAX_EXAMPLES)
+def test_oct2dec_and_dec2oct_are_inverses(octal_string):
+    assert octal_string == DEC2OCT(OCT2DEC(octal_string))
+
+
+# need to do:
+# bin2oct-oct2bin (done)
+# dec2bin-bin2dec (done)
+# hex2bin-bin2hex (done)
+# dec2oct-oct2dec (done)
+# oct2hex-hex2oct
+# hex2dec-dec2hex
+    
+
+# env_hex2oct = formula_env(HEX2OCT, "number")
+
+# def test_hex2oct(env_hex2oct):
+#     variables = given(
+#         number=one_of(
+#             xl_numbers(),
+#             hex_numbers_as_strings(3)
+#         )
+#     )
+#     fuzz_scalars(env=env_hex2bin, variables=variables)
+
+# # todo: put max parameter on the binary strings too
